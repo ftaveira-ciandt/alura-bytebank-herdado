66de0b7065eb5d24f0f7933067f451c2d1e63ea1
Francisco Taveira <ftaveira@ciandt.com>
2023-02-14 14:43:26 -0300

    [Alura PH 02] - reescrita de metodos

--- src/Gerente.java
+++ src/Gerente.java
@@ -15,9 +15,9 @@ public class Gerente extends Funcionario {
     }
 
 
-//    public double getBonificacao() {
-//        return this.salario;
-//    }
+    public double getBonificacao() {
+        return super.salario;
+    }
 
 
 
