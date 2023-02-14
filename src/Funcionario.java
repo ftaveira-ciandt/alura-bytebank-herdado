870d21e60d5830518660461ccddce8bacb61b5d2
Francisco Taveira <ftaveira@ciandt.com>
2023-02-14 11:48:33 -0300

    [Alura PH 01] - Criacao Classe Funcionario

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,3 +1,39 @@
 public class Funcionario {
 
+    private String nome;
+    private String cpf;
+    private double salario;
+
+    public double getBonificacao() {
+        return this.salario *0.1;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Funcionario setNome(final String nome) {
+        this.nome = nome;
+        return this;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Funcionario setCpf(final String cpf) {
+        this.cpf = cpf;
+        return this;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public Funcionario setSalario(final double salario) {
+        this.salario = salario;
+        return this;
+    }
+
 }
